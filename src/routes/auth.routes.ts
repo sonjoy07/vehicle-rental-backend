82bb040ff3de3f9dda61e86946e9bfc9367581a9
Sonjoy Biswas <sonjoywc@gmail.com>
2026-08-12 17:04:51 +0600

    add missing rate limiter

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,8 +2,9 @@ import { Router } from 'express';
 import { loginSchema } from '../validators/auth.validator';
 import { validate } from '../middlewares/validate';
 import { login } from '../controllers/AuthController';
+import { authRateLimiter } from '../middlewares/rateLimiter';
 
 const router = Router();
 
-router.post('/login', validate(loginSchema), login);
+router.post('/login',authRateLimiter, validate(loginSchema), login);
 export default router;
